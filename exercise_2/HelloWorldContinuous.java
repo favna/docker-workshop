73a2f29d0381a793e030dcaaadb9d6d1c0569e82
thomas.gribnau <thomas.gribnau@cgi.com>
2022-03-21 13:34:48 +0100

    refactor

--- exercise_2/HelloWorldContinuous.java
+++ exercise_2/HelloWorldContinuous.java
@@ -1,8 +1,6 @@
 public class HelloWorldContinuous {
     public static void main(String[] args) throws InterruptedException {
-        while (true) {
             System.out.println("Hello World");
-            Thread.sleep(1000);
         }
     }
 }
